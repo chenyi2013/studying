274921b28c9f0354915dc1ab9d26c968d5adb096
wangchi <wangchi0396@gmail.com>
2014-02-21 21:21:04 +0800

    add some grunt files

---
+++ grunt demo/css concat and compress/Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json');
+		concat: {
+			css: {
+				src: ['css/module/*.css'],
+				dest: 'css/pages/style.css'
+			}
+		},
+		cssmin: {
+			css: {
+				src: 'css/pages/style.css',
+				dest: 'css/pages/styel-min.css'
+			}
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-css');
+
+	grunt.registerTask('default', ['concat', 'cssmin']);
+};
