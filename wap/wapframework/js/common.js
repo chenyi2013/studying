ab21c1292986ab2907d5ba7aff108ecc78f49772
wangchi <wangchi0396@gmail.com>
2014-07-13 01:06:41 +0800

    modified wap

--- wap/wapframework/js/common.js
+++ wap/wapframework/js/common.js
@@ -1,9 +1,15 @@
 $(function () {
-	$('.j-menu-toggle').on('touchstart', function () {
+	$('.j-menu-toggle').on('click', function () {
 		$('html').addClass('menuShow');
+		//setTimeout(function () {
+			$('.menumask').css({
+				'background': 'rgba(255, 255, 255, 0.5)'
+				// 'opacity': '1'
+			});
+		//}, 500);
 	});
 
-	$('.menumask').on('touchstart', function () {
+	$('.menumask').on('click', function () {
 		$('html').removeClass('menuShow');		
 	})
 });
