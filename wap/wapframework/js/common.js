c15e6098350f73daf6cdafd7039c1b93b9a8dea0
wangchi <wangchi0396@gmail.com>
2014-07-13 11:40:42 +0800

    modified wapframework

--- wap/wapframework/js/common.js
+++ wap/wapframework/js/common.js
@@ -1,15 +1,19 @@
 $(function () {
 	$('.j-menu-toggle').on('click', function () {
 		$('html').addClass('menuShow');
-		//setTimeout(function () {
+/*		setTimeout(function () {
 			$('.menumask').css({
 				'background': 'rgba(255, 255, 255, 0.5)'
 				// 'opacity': '1'
 			});
-		//}, 500);
+		}, 500);*/
+	// $('.container').animate({'opacity': '0.5'}, '100');
+		// $('.container').css({'opacity': '0.5'});
 	});
 
 	$('.menumask').on('click', function () {
-		$('html').removeClass('menuShow');		
+		$('html').removeClass('menuShow');
+	// $('.container').animate({'opacity': '1'}, '100');
+
 	})
 });
