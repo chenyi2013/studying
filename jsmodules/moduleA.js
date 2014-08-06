889a0209e5f6573624e506fde9b3173193b6f861
wangchi <hiwangchi@gmail.com>
2014-08-06 17:20:16 +0800

    modified jsmodules

--- jsmodules/moduleA.js
+++ jsmodules/moduleA.js
@@ -1,8 +1,13 @@
-/*
 
-var moduleA = {
+
+// === 001
+
+/*var moduleA = {
 	
+	id: 111,
+
 	a_1: function () {
+		console.log('a_1--id:'+this.id);
 		console.log('a_1');
 	},
 
@@ -12,16 +17,21 @@ var moduleA = {
 
 };
 
-moduleA.a_1 = '4';
-
+console.log(moduleA.id); //111
+moduleA.id = 99;//此时内部a_1函数里输出的id已经变为了99
+//暴露了内部的所有模块成员, 内部数据可以被外部改写
 */
 
 
+
+
+// === 002
 var moduleA = (function () {
 
-	var id = 001;
+	var id = 111;
 
 	var a_1 = function () {
+		console.log('a_1--id:'+id);
 		console.log('a_1');
 	};
 
@@ -30,8 +40,12 @@ var moduleA = (function () {
 	};
 
 	return {
-		a1: a_1,
-		a2: a_2
+		a_1: a_1,
+		a_2: a_2
 	};
 
 })();
+
+console.log( moduleA.id ); //undefine
+moduleA.id = 99;//此时内部a_1函数里输出的id仍然为111, 而不是99
+//有效地保护了内部成员变量
