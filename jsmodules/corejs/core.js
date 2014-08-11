cbb6c7acdbebd53992829c99a12e5634bc7178d7
wangchi <wangchi0396@gmail.com>
2014-08-12 00:50:37 +0800

    modified corejs

--- jsmodules/corejs/core.js
+++ jsmodules/corejs/core.js
@@ -1,4 +1,80 @@
-var Core = (function () {
+(function () {
+
+	function Core ( ele ) {
+		return new Core.fn.init( ele );	
+	}
+
+	Core.prototype.css = function () {
+		console.log('css');
+	}
+
+	init = Core.prototype.init = function ( ele ) {
+		var elements = [];
+		elements.push( document.getElementById( ele ) );
+		// return this;
+	}
+
+	init.prototype = Core.fn;
+
+	Core.fn = Core.prototype = {
+		sayHi: function () {
+			console.log('Hi');
+		}
+	}
+/*
+	function $ ( ele ) {
+		return new Core( ele );
+	}*/
+
+
+	window.Core = Core;
+
+})();
+
+
+/*var Core = (function () {
+
+	var core = {};
+
+	core.selector = function ( ele ) {
+		var elements = [];
+		elements.push( document.getElementById( ele ) );
+	}
+
+	core.prototype = {
+		// constructor: Core,
+		sayHi: function () {
+			console.log('Hi');
+		}
+	};
+
+	function $ ( ele ) {
+		return core;
+	}
+
+})( Core );*/
+
+/*var Core = function ( ele ) {
+	var elements = [];
+	elements.push( document.getElementById( ele ) );	
+}
+*/
+/*function Core ( elements ) {
+	// console.log('Core');
+	this.elements = [];
+	this.elements.push( document.getElementById( elements ) );
+
+	//return this.elements;
+}*/
+
+// window.Core = window.$ = Core;
+
+
+
+/*
+// function 
+
+//var Core = (function () {
 	var $;
 
 	Core = function ( elements ) {
@@ -7,23 +83,25 @@ var Core = (function () {
 
 	//new Core( elements );
 
-	Core.prototype.sayHi = function () {
-		alert('Hi');
+	Core.fn = Core.prototype = {
+		sayHi: function () {
+			console.log('Hi');
+		}
 	}
 
-/*	Core.fn = Core.prototype = function () {
+	Core.fn.init = function () {
+		return this;
+	}
 
-	}*/
+	// Core.prototype.constructor = Core;
 
-/*	function $ ( elements ) {
-		reuturn Core;
-	}*/
+
 
 	//$ = core;
 	// $.sayHi = core.sayHi;
 
 	window.Core = window.$ = Core;
 
-	return Core;
+	//return Core;
 
-})(Core);
+//})(Core);*/
