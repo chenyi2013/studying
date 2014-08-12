6560447e40339ecbf4128ee13fe2d2e962e6190a
wangchi <hiwangchi@gmail.com>
2014-08-12 18:38:51 +0800

    add js module jsontools (function jsonToStr)

---
+++ requirejs/002-use/js/modules/jsontools.js
@@ -0,0 +1,14 @@
+/*
+* json操作
+*/
+define(function () {
+	return {
+		jsonToStr: function ( json ) {
+			var result = '{';
+			for ( var i in json ) {
+				result += "'" + i + "'" + ": '" + json[i] + "', ";
+			}
+			return (result + '}').replace(', }', '}');
+		}
+	};
+});
