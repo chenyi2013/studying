bcb1af27c7ace4b8feb3dcab02659aaf64702173
wangchi <wangchi0396@gmail.com>
2014-09-10 23:57:49 +0800

    modified pubuliu02 style

--- pubuliu/js/pubuliu02.js
+++ pubuliu/js/pubuliu02.js
@@ -1,7 +1,16 @@
 var pubuliu02 = (function ($) {
 	return {
 		init: function () {
-			console.log($('.list').length);
+			var _uList = $('.lists'),
+				_list = _uList.find('.list'),
+
+				marginL = 10;
+
+			_list.eq(0).css({'left': marginL});
+			_list.eq(1).css({'left': marginL*3 + _list.eq(0).width() });
+			_list.eq(2).css({'left': marginL*5 + _list.eq(0).width()*2 });
+			_list.eq(3).css({'left': marginL*7 + _list.eq(0).width()*3 });
+			_list.eq(4).css({'left': marginL*9 + _list.eq(0).width()*4 });
 		}
 	};
 })(jQuery);
