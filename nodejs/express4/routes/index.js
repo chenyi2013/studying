fc12e166d1889de9a59f050379c399f04252aa4d
wangchi <hiwangchi@gmail.com>
2014-09-29 19:25:30 +0800

    express

---
+++ nodejs/express4/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: '加意新品' });
+});
+
+router.get('/login', function ( req, res ) {
+	res.render('login', { title: '登录' });
+});
+
+router.get('/home', function ( req, res ) {
+	res.render('home', { title: '用户中心' });
+});
+
+module.exports = router;
