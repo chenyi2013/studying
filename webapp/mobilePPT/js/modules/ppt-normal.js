a11df8aaed32d375b23766b067d6dc960d07a2fe
wangchi <hiwangchi@gmail.com>
2014-12-19 12:39:44 +0800

    update ppt-normal

--- webapp/mobilePPT/js/modules/ppt-normal.js
+++ webapp/mobilePPT/js/modules/ppt-normal.js
@@ -9,16 +9,38 @@ var pptNormal = (function () {
         pageHeight = $('body').height()
         ;
 
-
+    // 初始化
     function init () {
         $(window).on('load', function () {
-
             ppts.height( pageHeight );
-
         });
     }
 
+    // ppt切换 上下切换
+    function pptSwitch () {
+        for (var i = 0, len = ppts.length; i < len; i++) {
+            (function (i) {
+
+                if ( i < len - 1 ) {
+                    $(ppts[i]).swipeUp(function () {
+                        mobilePPT[0].style.webkitTransform = 'translateY(' + (-pageHeight)*(i+1) + 'px)';
+                    });
+                }
+
+                if ( i > 0 ) {
+                    $(ppts[i]).swipeDown(function () {
+                        mobilePPT[0].style.webkitTransform = 'translateY(' + (-pageHeight)*(i-1) + 'px)';
+                    });
+                }
+
+            })(i);
+        }
+    }
+
+
+
     return {
-        init: init
+        init: init,
+        switch: pptSwitch
     };
 })(Zepto);
