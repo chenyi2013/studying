ecffd4e465b2e2a82aa7735e631b1bda287651b6
wangchi <hiwangchi@gmail.com>
2015-02-01 20:47:45 +0800

    REST API

--- backbone/Developing-Backbone.js-Applications-notes/05/BookLibrary/server.js
+++ backbone/Developing-Backbone.js-Applications-notes/05/BookLibrary/server.js
@@ -41,22 +41,30 @@ app.configure(function () {
 // database
 mongoose.connect('mongodb://localhost/library_database');
 
+
+
 // schemas
+var Keywords = new mongoose.Schema({
+    keyword: String
+});
+
 var Book = new mongoose.Schema({
     title: String,
     author: String,
-    releaseDate: Date
+    releaseDate: Date,
+    keywords: [Keywords]
 });
 
 var BookModel = mongoose.model( 'Book', Book );
 
 
 
-
+// test
 app.get('/api', function ( req, res ) {
     res.send('Library api is running!');
 });
 
+// 返回所有图书
 app.get('/api/books', function ( req, res ) {
     return BookModel.find(function ( err, books ) {
         if (!err) {
@@ -67,12 +75,16 @@ app.get('/api/books', function ( req, res ) {
     });
 });
 
+// 添加数据
 app.post('/api/books', function ( req, res ) {
     var book = new BookModel({
         title: req.body.title,
         author: req.body.author,
-        releaseDate: req.body.releaseDate
+        releaseDate: req.body.releaseDate,
+        keywords: req.body.keywords
     });
+
+    console.log(book.keywords);
 
     book.save(function ( err ) {
         if (!err) {
@@ -83,6 +95,54 @@ app.post('/api/books', function ( req, res ) {
     });
 
     return res.send(book);
+});
+
+// 返回单本图书
+app.get('/api/books/:id', function ( req, res ) {
+    return BookModel.findById( req.params.id, function ( err, book ) {
+        if (!err) {
+            return res.send( book );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+
+
+// update a book
+app.put('/api/books/:id', function ( req, res ) {
+    console.log( req.body.title );
+    return BookModel.findById( req.params.id, function ( err, book ) {
+        book.title = req.body.title;
+        book.author= req.body.author;
+        book.releaseDate = req.body.releaseDate;
+        book.keywords = req.body.keywords;
+
+        return book.save(function ( err ) {
+            if (!err) {
+                console.log('book updated');
+            } else {
+                console.log(err);
+            }
+            return res.send( book );
+        });
+    });
+});
+
+
+// delete a book
+app.delete('/api/books/:id', function ( req, res ) {
+    console.log('Deleting book with id:' + req.params.id);
+    return BookModel.findById( req.params.id, function ( err, book ) {
+        return book.remove(function (err) {
+            if (!err) {
+                console.log('Book removed');
+                return res.send(book);
+            } else {
+                console.log( err );
+            }
+        });
+    });
 });
 
 
