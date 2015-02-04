5f5b2db7e00e021aab54d63765e40f2a20df9c86
wangchi <hiwangchi@gmail.com>
2015-02-04 15:19:16 +0800

    Initial Commit

---
+++ Objective-C/03.01 Count-1/03.01 Count-1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  03.01 Count-1
+//
+//  Created by 王驰 on 15/2/2.
+//  Copyright (c) 2015年 Walker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main ( int argc, const char *argv[] ) {
+    NSLog(@"The numbers from 1 to 5:");
+    
+    for ( int i = 1; i <= 5; i++) {
+        NSLog(@"%d\n", i);
+    }
+    
+    return (0);
+}
---
+++ Objective-C/WebViewTest/WebViewTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebViewTest
+//
+//  Created by 王驰 on 15/2/4.
+//  Copyright (c) 2015年 Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
